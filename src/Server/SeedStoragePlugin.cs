e3226e1b213d25a7d756c54f76f34bfc8456d8b1
jcdcdev <james@jcdc.dev>
2023-10-09 18:35:00 +0100

    fix: issue with shelf life multiplier not working

--- src/Server/SeedStoragePlugin.cs
+++ src/Server/SeedStoragePlugin.cs
@@ -1,13 +1,5 @@
-﻿using Eco.Shared.Localization;
-using jcdcdev.Eco.Core;
+﻿using jcdcdev.Eco.Core;
 
 namespace jcdcdev.Eco.SeedStorage;
 
-public class SeedStoragePlugin : PluginBase<SeedStorageConfig>
-{
-    protected override void BuildStatusText(LocStringBuilder sb)
-    {
-        sb.AppendLine(new LocString($"Seed Bank Shelf Life Multiplier: {Config.SeedBankShelfLifeMultiplier}"));
-        sb.AppendLine(new LocString($"Wooden Seed Box Shelf Life Multiplier: {Config.WoodenSeedBoxShelfLifeMultiplier}"));
-    }
-}
+public class SeedStoragePlugin : PluginBase { }
